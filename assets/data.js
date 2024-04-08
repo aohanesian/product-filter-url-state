9496b9642b2106232c32df40922c2cf819475610
aohanesian <103946712+aohanesian@users.noreply.github.com>
2024-04-08 23:40:31 +0200

    init

---
+++ assets/data.js
@@ -0,0 +1,213 @@
+const data = [
+    {
+        id: 1,
+        name: "Curren Grass",
+        img: "assets/291020394.webp",
+        price: 695,
+        cat: "Dress"
+    },
+    {
+        id: 2,
+        name: "Curren Grass Brown",
+        img: "assets/291020395.webp",
+        price: 695,
+        cat: "Dress"
+    },
+    {
+        id: 3,
+        name: "CASIO Edifice EF-539D-1AVEF",
+        img: "assets/341002416.webp",
+        price: 8820,
+        cat: "Luxury"
+    },
+    {
+        id: 4,
+        name: "Poedagar Nice",
+        img: "assets/320148375.webp",
+        price: 989,
+        cat: "Dress"
+    },
+    {
+        id: 5,
+        name: "GoldenHour Water",
+        img: "assets/291021478.webp",
+        price: 2700,
+        cat: "Sport"
+    },
+    {
+        id: 6,
+        name: "CASIO G-Shock GBD-200-1ER",
+        img: "assets/318370967.webp",
+        price: 8690,
+        cat: "Sport"
+    },
+    {
+        id: 7,
+        name: "North Edge Gavia 20BAR",
+        img: "assets/291024791.webp",
+        price: 6899,
+        cat: "Luxury"
+    },
+    {
+        id: 8,
+        name: "Carnival Genius",
+        img: "assets/291021024.webp",
+        price: 3300,
+        cat: "Luxury"
+    },
+    {
+        id: 9,
+        name: "Naviforce Pride",
+        img: "assets/291024702.webp",
+        price: 1200,
+        cat: "Luxury"
+    },
+    {
+        id: 10,
+        name: "CASIO MW-240-1EVEF",
+        img: "assets/318370731.webp",
+        price: 1530,
+        cat: "Dress"
+    },
+    {
+        id: 11,
+        name: "CASIO F-91WM-7AEF",
+        img: "assets/332138257.webp",
+        price: 1420,
+        cat: "Casual"
+    },
+    {
+        id: 12,
+        name: "CASIO G-Shock GBD-200UU-9ER",
+        img: "assets/340359293.webp",
+        price: 8690,
+        cat: "Sport"
+    },
+    {
+        id: 13,
+        name: "Besta Symbol",
+        img: "assets/329071743.webp",
+        price: 977,
+        cat: "Sport"
+    },
+    {
+        id: 14,
+        name: "CASIO MTP-1259PD-1AEG",
+        img: "assets/313868519.webp",
+        price: 2740,
+        cat: "Casual"
+    },
+    {
+        id: 15,
+        name: "CASIO LTP-1129PA-7BEF/LTP-1129A-7BEF",
+        img: "assets/46459811.webp",
+        price: 1790,
+        cat: "Casual"
+    },
+    {
+        id: 16,
+        name: "HANOWA HAWLG0001560",
+        img: "assets/344764165.webp",
+        price: 7960,
+        cat: "Dress"
+    },
+    {
+        id: 17,
+        name: "HANOWA HAWLH0001201",
+        img: "assets/344764167.webp",
+        price: 7960,
+        cat: "Luxury"
+    },
+    {
+        id: 18,
+        name: "Victorinox Swiss Army I.N.O.X. Mechanical V241837",
+        img: "assets/173889294.webp",
+        price: 2983,
+        cat: "Luxury"
+    },
+    {
+        id: 19,
+        name: "Emporio Armani AR11406",
+        img: "assets/298118447.webp",
+        price: 9600,
+        cat: "Casual"
+    },
+    {
+        id: 20,
+        name: "CASIO LTP-1154PE-7BEF",
+        img: "assets/46084097.webp",
+        price: 2150,
+        cat: "Casual"
+    },
+    {
+        id: 21,
+        name: "GUESS GW0405L1",
+        img: "assets/286479104.webp",
+        price: 9300,
+        cat: "Dress"
+    },
+    {
+        id: 22,
+        name: "Naviforce Malibu",
+        img: "assets/325881807.webp",
+        price: 895,
+        cat: "Dress"
+    },
+    {
+        id: 23,
+        name: "Michael Kors MK4568",
+        img: "assets/293724028.webp",
+        price: 5800,
+        cat: "Luxury"
+    },
+    {
+        id: 24,
+        name: "TIMEX Tx2n677",
+        img: "assets/10612728.webp",
+        price: 3232,
+        cat: "Casual"
+    },
+    {
+        id: 25,
+        name: "CASIO WS-1500H-1A",
+        img: "assets/329433886.webp",
+        price: 2440,
+        cat: "Sport"
+    },
+    {
+        id: 26,
+        name: "CASIO G-Shock GA-2110ET-8AER",
+        img: "assets/316545411.webp",
+        price: 6330,
+        cat: "Sport"
+    },
+    {
+        id: 27,
+        name: "Daniel Wellington",
+        img: "assets/346701997.webp",
+        price: 7880,
+        cat: "Dress"
+    },
+    {
+        id: 28,
+        name: "Casio G-Shock GBA-900-1AER",
+        img: "assets/354375512.webp",
+        price: 7580,
+        cat: "Sport"
+    },
+    {
+        id: 29,
+        name: "Victorinox Swiss Army Alliance",
+        img: "assets/306098813.webp",
+        price: 6570,
+        cat: "Casual"
+    },
+    {
+        id: 30,
+        name: "CASIO A120WEGG-1BEF",
+        img: "assets/350267964.webp",
+        price: 5610,
+        cat: "Casual"
+    },
+];
+export default data;
---
+++ script.js
@@ -0,0 +1,168 @@
+import data from "./assets/data.js";
+
+const productsContainer = document.querySelector('.products');
+const searchInput = document.querySelector('.search');
+const categoriesContainer = document.querySelector('.cats');
+const priceRange = document.querySelector('.priceRange');
+const priceValue = document.querySelector('.priceValue');
+const ascendingBtn = document.querySelector('#ascending');
+const descendingBtn = document.querySelector('#descending');
+
+let filterSettings = {
+    catSelected: 'All',
+    priceSelected: getMaxPrice(),
+    sortOrder: 'default',
+    isSearch: false,
+};
+
+function initApp() {
+    loadFilterSettingsFromURL();
+    setPrices();
+    setCategories();
+    applyFilters();
+    displayProducts(data);
+};
+initApp();
+
+// Event listeners
+priceRange.addEventListener('input', handlePriceRangeChange);
+searchInput.addEventListener('input', handleSearchInputChange);
+categoriesContainer.addEventListener('click', handleCategoryClick);
+ascendingBtn.addEventListener('click', handleSortOrderChange);
+descendingBtn.addEventListener('click', handleSortOrderChange);
+
+window.addEventListener('popstate', () => {
+    // Reload filter settings from URL when browser history changes
+    loadFilterSettingsFromURL();
+    // Apply filters based on the updated filter settings
+    applyFilters();
+});
+
+function handlePriceRangeChange(e) {
+    priceValue.textContent = e.target.value + ' UAH';
+    filterSettings.priceSelected = +e.target.value;
+    applyFilters();
+}
+
+function handleSearchInputChange(e) {
+    const value = e.target.value.toLowerCase();
+    filterSettings.isSearch = !!value;
+    updateURL();
+    applyFilters();
+}
+
+function handleCategoryClick(e) {
+    const selectedCat = e.target.textContent;
+    if (e.target.tagName === 'DIV') return;
+    filterSettings.catSelected = selectedCat;
+    updateURL();
+    applyFilters();
+}
+
+function handleSortOrderChange() {
+    filterSettings.sortOrder = this.id;
+    updateURL();
+    applyFilters();
+}
+
+function applyFilters() {
+    let filteredProducts = data;
+    const value = searchInput.value.toLowerCase();
+
+    if (filterSettings.isSearch) {
+        filteredProducts = data.filter(
+            (item) => item.name.toLowerCase().indexOf(value) !== -1
+        );
+    }
+
+    if (filterSettings.catSelected !== 'All') {
+        filteredProducts = filteredProducts.filter(
+            (item) => item.cat === filterSettings.catSelected
+        );
+    }
+
+    filteredProducts = filteredProducts.filter(
+        (item) => item.price <= filterSettings.priceSelected
+    );
+
+    if (filterSettings.sortOrder === 'ascending') {
+        filteredProducts = filteredProducts.sort((a, b) => a.price - b.price);
+    } else if (filterSettings.sortOrder === 'descending') {
+        filteredProducts = filteredProducts.sort((a, b) => b.price - a.price);
+    }
+
+    displayProducts(filteredProducts);
+}
+
+function displayProducts(filteredProducts) {
+    if (filteredProducts.length) {
+        productsContainer.innerHTML = filteredProducts
+            .map(
+                (product) => `
+                <div class="product">
+                    <img draggable="false" alt="watches" src=${product.img}>
+                    <span class="name">${product.name}</span>
+                    <span class="priceText">${product.price} UAH</span>
+                </div>
+            `
+            )
+            .join('');
+    } else {
+        productsContainer.innerHTML = `<span>NO DATA</span>`;
+    }
+}
+
+function setPrices() {
+    const priceList = data.map((item) => item.price);
+    const minPrice = Math.min(...priceList);
+    const maxPrice = Math.max(...priceList);
+
+    priceRange.min = minPrice;
+    priceRange.max = maxPrice;
+    priceRange.value = filterSettings.priceSelected;
+    priceValue.textContent = filterSettings.priceSelected + ' UAH';
+}
+
+function getMaxPrice() {
+    const priceList = data.map((item) => item.price);
+    return Math.max(...priceList);
+}
+
+function setCategories() {
+    const allCats = data.map((item) => item.cat);
+    const categories = ['All', ...new Set(allCats)];
+    categoriesContainer.innerHTML = categories
+        .map(
+            (cat) =>
+                `
+          <span class="cat">${cat}</span>
+      `
+        )
+        .join('');
+}
+
+function loadFilterSettingsFromURL() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('catSelected')) {
+        filterSettings.catSelected = urlParams.get('catSelected');
+    }
+    // if (urlParams.has('priceSelected')) {
+    //     filterSettings.priceSelected = parseInt(urlParams.get('priceSelected'));
+    // }
+    if (urlParams.has('sortOrder')) {
+        filterSettings.sortOrder = urlParams.get('sortOrder');
+    }
+    if (urlParams.has('isSearch')) {
+        filterSettings.isSearch = urlParams.get('isSearch') === 'true';
+    }
+}
+
+function updateURL() {
+    const urlParams = new URLSearchParams();
+    urlParams.set('catSelected', filterSettings.catSelected);
+    // urlParams.set('priceSelected', filterSettings.priceSelected);
+    urlParams.set('sortOrder', filterSettings.sortOrder);
+    urlParams.set('isSearch', filterSettings.isSearch);
+    const newURL = window.location.pathname + '?' + urlParams.toString();
+    window.history.pushState({}, '', newURL);
+}
